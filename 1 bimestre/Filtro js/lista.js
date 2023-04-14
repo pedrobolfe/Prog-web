37f64a73769a9d479170a68d828c322e84e61d2a
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-04-14 11:52:26 -0300

    Update lista.js

--- 1 bimestre/Filtro js/lista.js
+++ 1 bimestre/Filtro js/lista.js
@@ -2,12 +2,13 @@ const nameList = ["Manoel Gomes", "Fernandão", "Nareba", "BaraBaraBara", "TI"];
 
 const searchField = document.getElementById("searchField");     // barra de pesquisa
 const listEl = document.getElementById("list");     // nomes filtrados
-
 const btFiltrar = document.getElementById("btFiltrar");     // botao filtrar
 const btAdicionar = document.getElementById("btAdicionar");     //  botao adicionar
 const btSelecionar = document.getElementById("btSelecionar");     //  botao selecionar
 const btRemover = document.getElementById("btRemover");    // botao remover
 const saida = document.getElementById("saida");     // saida para mostra umas msg
+const nomeAux = nameList;
+var aux = -1;
 
 this.fillList();
 
@@ -15,6 +16,11 @@ function fillList(list = nameList) {
     listEl.innerHTML = "";
     for(let i = 0; i < list.length; i++) {
         let listItems = document.createElement("li");
+        if (aux == i){
+            listItems.className = "alert-link";
+        } else{
+            listItems.className = "";
+        }
         listItems.innerHTML = list[i];
         listEl.appendChild(listItems);
     }
@@ -31,7 +37,8 @@ function inputHandler() {
 }
 
 function adicionar_item(){
-    item = searchField.value.toLowerCase();    //  'item' recebe o valor da barra de pesquisa e transforma em minusculo
+    aux = 0;
+    item = searchField.value;    //  'item' recebe o valor da barra de pesquisa
     if (item != ''){    //  se a barra de pesquisa nao estiver vazia, o nome sera add na lista 
         if (nameList.indexOf(item) == -1){  // se o item nao estiver na lista, o item sera adicionado
             nameList.push(item);    //  adciona o nome na ultima posicao da lista
@@ -41,6 +48,7 @@ function adicionar_item(){
 }
 
 function remover_item(){
+    aux = 0;
     item = searchField.value.toLowerCase();     //  'nome' recebe o valor da barra de pesquisa e transforma em minusculo
     var verifica = nameList.indexOf(item);     // indexOf verifica se tem um item em uma lista, se for -1 e pq n esta na lista, se estiver ele retorna a posicao do item
     if (verifica != -1) {   // se for diferente de -1 e pq o item esta na lista
@@ -49,35 +57,12 @@ function remover_item(){
     }
 }
 
-// frm.btSelecionar.addEventListener("click", () => 
-function selecionar_item(){ 
-    const lista_nomes = document.querySelectorAll("li");  // obtém tags li da página
-    console.log(lista_nomes);
-
-    if (lista_nomes.length == 0) {
-        alert("Não há nomes para selecionar");       // se não há tarefas, exibe alerta
-        return                                        // e retorna
-    }
-  
-    let aux = -1                   // variável auxiliar para indicar linha selecionada
-  
-    // percorre a lista de elementos h5 inseridos na página, ou seja, tarefas
-    for (let i = 0; i < nameList.length; i++) {
-      // se tag é da class tarefa-selecionada (está selecionada)
-      if (lista_nomes[i].className == "nome-selecionada") {
-        lista_nomes[i].className = "nome-normal"      // troca para normal
-        aux = i                                     // muda valor da variável auxiliar
-        break                                       // sai da repetição
-      }
-    }
-  
-    // se a linha que está selecionada é a última, irá voltar para a primeira
-    if (aux == lista_nomes.length - 1) {
-      aux = -1
-    }
-  
-    lista_nomes[aux + 1].className = "nome-selecionada" // muda estilo da próxima linha
-  }
+function selecionar_item(){
+    if (nameList.length == aux){
+        aux = 0;
+    } else { aux += 1;}
+    inputHandler();
+}
 
 btSelecionar.addEventListener("click", selecionar_item); // chama a funcao quando o botao for clicado
 btFiltrar.addEventListener("click", inputHandler);  //                 ||
